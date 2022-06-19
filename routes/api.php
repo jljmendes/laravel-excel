8ab86fbd1cf6ebf8620456b9e974e7c469d5f224
Jorge Mendes <jljmendesmanutencao@gmail.com>
2022-06-18 23:57:29 -0300

    Alterando a cor do fndo

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,37 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/upload', function(Request $request){
+    $array = ['error' => ''];
+
+    $rules = [
+        'name' => 'required|min:2',
+        'arquivo' => 'required|mimes:xls,xmls,csv,xlsx'
+    ];
+
+    $validator = Validator::make($request->all(), $rules);
+
+    if($validator->fails()){
+        $array['error'] = $validator->messages();
+        return $array;
+    }
+
+    if($request->hasFile('arquivo')){
+        if($request->file('arquivo')->isValid()){
+
+            $arquivo = $request->file('arquivo')->store('public');
+            $url = asset(Storage::url($arquivo));
+            echo $url;
+        }
+    }else{
+        $array['error'] = 'Nao foi enviado arquivo';
+    }
+    // $name = $request->input('name');
+    // $arquivo = $request->file('arquivo');
+
+
+
+    return $array;
 });
